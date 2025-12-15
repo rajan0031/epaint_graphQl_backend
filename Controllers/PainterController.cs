0b9d62ba7ecfa116e1a9166355ee699319eef80a
rajan0031 <2403951@cognizant.com>
2025-12-15 16:51:00 +0530

    upto add painter is  done, with role based authentication

--- Controllers/PainterController.cs
+++ Controllers/PainterController.cs
@@ -1,6 +1,7 @@
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MyGraphqlApp.Exception.UserException;
 using MyGraphqlApp.Model;
 
 namespace MyGraphqlApp.Controllers.PainterController
@@ -14,17 +15,24 @@ namespace MyGraphqlApp.Controllers.PainterController
     {
 
         private readonly IpaintService _ipaintService;
+        private readonly JwtUtils _jwtUtils;
 
 
-        public PainterController(IpaintService ipaintService)
+        public PainterController(IpaintService ipaintService, JwtUtils jwtUtils)
         {
             _ipaintService = ipaintService;
+            _jwtUtils = jwtUtils;
         }
 
         [Authorize]
         [HttpPost("register")]
         public Task<Painter> RegisterPainter([FromBody] Painter painter)
         {
+            var userDetails = _jwtUtils.ExtractLoggedInUserDetails();
+            if (userDetails.Role != 1)
+            {
+                throw new UserException("Unauthorize access , you are not Authroize to this resourse",System.Net.HttpStatusCode.Unauthorized);
+            }
             var result = _ipaintService.RegisterPainter(painter);
             return result;
         }
